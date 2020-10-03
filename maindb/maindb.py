144819552100057855fa1cfaf91840f5ee9b1306
Deborah Liske <del073@g.harvard.edu>
2020-10-03 05:43:08 -0400

    initial commit

--- maindb/maindb.py
+++ maindb/maindb.py
@@ -8,17 +8,28 @@ app = Flask(__name__)
 @app.route("/", methods=["POST", "GET"])
 def mainm():
 
+    # if request.method == "POST":
+    #     filename = request.get_json()["art_image"]
+    #     meta = get_metadata(filename)
+    #     google_features = get_google_feature(filename)
+    #     dominant_colors = get_dominant_color(filename)
+    #     nearest = get_nearest(filename)
+    #     msg = "\nLoading filename successful! Now we are at root:" + os.getcwd()
+
+    #     return render_template(
+    #         "display.html",
+    #         art_image=filename,
+    #         msg=msg,
+    #         google_features=str(google_features),
+    #         dominant_colors=str(dominant_colors),
+    #         nearest_images=str(nearest),
+    #     )
+
     if request.method == "POST":
         filename = request.get_json()["art_image"]
-        meta = get_metadata(filename)
-        google_features = get_google_feature(filename)
-        dominant_colors = get_dominant_color(filename)
-        nearest = get_nearest(filename)
         msg = "\nLoading filename successful! Now we are at root:" + os.getcwd()
+        return render_template("display.html", art_image=filename, msg=msg)
 
-        return render_template("display.html", art_image=filename, msg=msg,
-                                google_features=str(google_features), dominant_colors=str(dominant_colors),
-                                nearest_images=str(nearest))
     else:
         return "maindb.py - This is get method - try using post -- "
 
